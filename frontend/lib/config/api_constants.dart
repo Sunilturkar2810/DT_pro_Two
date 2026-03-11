6ad192b24f299a56e20a7f2b7918675e6338f3bd
Sunil Turkar <sunilturkar2810@gmail.com>
2026-03-11 16:11:21 +0530

    chore: update api baseUrl to render production url

--- frontend/lib/config/api_constants.dart
+++ frontend/lib/config/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://192.168.1.23:5000/api";
+  static const String baseUrl = "https://dt-pro-two.onrender.com/api";
 
   // -- Auth ----------------------
   static const String login = '/auth/login';
